ffc5977bbdf928ca588ea6e22eabd0c59727c6b7
zgcc <xgt3890@sina.cn>
2024-12-16 15:42:54 +0800

    establish connection

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 
 using namespace std;
 
+pqxx::connection localConnection(
+        "dbname=postgres user=dboper password=dboper@321 host=120.46.71.168 port=26000 options='-c client_encoding=UTF8'");
+pqxx::connection cloudConnection(
+        "dbname=SMUMS057 user=dboper password=dboper@321 host=120.46.71.168 port=26000 options='-c client_encoding=UTF8'");
+pqxx::result res;
+
 int main() {
 
 }
---
+++ ustil.h
@@ -0,0 +1,13 @@
+//
+// Created by ZGCC on 24-12-16.
+//
+
+#ifndef CPP_SE_USTIL_H
+#define CPP_SE_USTIL_H
+
+extern pqxx::connection localConnection;
+extern pqxx::connection cloudConnection;
+extern pqxx::work txn;
+extern pqxx::result res;
+
+#endif //CPP_SE_USTIL_H
